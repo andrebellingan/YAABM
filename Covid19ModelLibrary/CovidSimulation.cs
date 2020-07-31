2a813ba643d51c2fbb15bc0896fe931617ec5d1f
andrebellingan <68418615+andrebellingan@users.noreply.github.com>
2020-07-31 12:33:12 +0200

    Load geography information from csv data file

--- Covid19ModelLibrary/CovidSimulation.cs
+++ Covid19ModelLibrary/CovidSimulation.cs
@@ -1,34 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Covid19ModelLibrary.Geography;
 using Covid19ModelLibrary.MultiState;
 using Covid19ModelLibrary.Population;
+using Serilog;
 using Yaabm.generic;
 
 namespace Covid19ModelLibrary
 {
-    public class CovidSimulation : Simulation<Human, CovidStateModel, CovidContext, CovidPopulation, CovidSimulation>
+    public class CovidSimulation : Simulation<Human, CovidStateModel, Ward, CovidPopulation, CovidSimulation>
     {
         public CovidSimulation(int seed, int iterationNo, CovidModelParameters parameters) : base(parameters.StartDate, iterationNo, seed, false)
         {
+            InitializeContext(parameters.WardFile, parameters);
+            InitializeHealthCareSystem(parameters); // Need to do this after the geography has been specified
             InitializePopulation(parameters);
+        }
+
+        private void InitializeHealthCareSystem(CovidModelParameters parameters)
+        {
+            Log.Warning("Healthcare system initialization is not implemented!");
+        }
+
+        private void InitializeContext(string wardFile, CovidModelParameters parameters)
+        {
+            var wardRecords = WardRecord.LoadFromCsv(wardFile);
+
+            AddRegions(wardRecords, w => new RegionSpec("root", w.CountryCode, w.CountryName, "Country"));
+            AddRegions(wardRecords, w => new RegionSpec(w.CountryCode, w.ProvinceCode, w.ProvinceName, "Province"));
+            AddRegions(wardRecords, w => new RegionSpec(w.ProvinceCode, $"dm_{w.DistrictMunicipalityCode}", w.DistrictMunicipalityName, "DistrictMunicipality")); // Add "dm_" prefix because of duplicate district and local municipality codes
+            AddRegions(wardRecords, w => new RegionSpec($"dm_{w.DistrictMunicipalityCode}", w.LocalMunicipalityCode, w.LocalMunicipalityName, "LocalMunicipality"));
+
+            foreach (var ward in wardRecords)
+            {
+                var newWard = new Ward(new BasicHospitalSystem(999), ward.WardId, parameters); // Get the appropriate hospital for this region
+                AddLocalArea(ward.LocalMunicipalityCode, newWard);
+            }
+        }
+
+
+
+        private void AddRegions(IEnumerable<WardRecord> wardRecords, Func<WardRecord, RegionSpec> regionFunc)
+        {
+            var regionsToAdd = wardRecords.Select(regionFunc).Distinct();
+
+            foreach (var region in regionsToAdd)
+            {
+                Log.Logger.Debug($"Adding region {region.LogString()}");
+                AddRegion(region);
+            }
         }
 
         private void InitializePopulation(CovidModelParameters parameters)
         {
-            foreach (var province in (Province[]) Enum.GetValues(typeof(Province)))
-            {
-                var provinceHospitalSystem = new BasicHospitalSystem(parameters.HospitalBedsPerThousand);
-                var provinceContext = new CovidContext(provinceHospitalSystem, province, RootContext, parameters.Clone());
-
-                AddLocalContext("root", provinceContext);
-            }
-
-            var populationPyramid = PopulationPyramid.LoadFromFile(parameters.PopulationFile);
-
-            populationPyramid.GeneratePopulation(parameters.NumberOfAgentsToSimulate, this);
-            PopulationDynamics.GenerateContacts(parameters.ContactMatrix);
+            throw new NotImplementedException("Need to generate the population");
         }
 
-        protected override void UpdateLocalContext(CovidContext asLocal)
+        protected override void UpdateLocalContext(Ward asLocal)
         {
             /*
              * THIS CODE IS VERY, VERY WRONG
