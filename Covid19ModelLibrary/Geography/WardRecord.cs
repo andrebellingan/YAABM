5009e4dadb954ce81b961526f2c3ac10edac0767
andrebellingan <68418615+andrebellingan@users.noreply.github.com>
2020-07-31 12:29:33 +0200

    Load geographic information from CSV data file

---
+++ Covid19ModelLibrary/Geography/WardRecord.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using CsvHelper;
+using Serilog;
+
+namespace Covid19ModelLibrary.Geography
+{
+    internal class WardRecord
+    {
+        public int ObjectId { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string CountryName { get; set; }
+
+        public string ProvinceCode { get; set; }
+
+        public string ProvinceName { get; set; }
+
+        public string LocalMunicipalityCode { get; set; }
+
+        public int WardNumber { get; set; }
+
+        public int WardId { get; set; }
+
+        public string LocalMunicipalityName { get; set; }
+
+        public string DistrictMunicipalityCode { get; set; }
+
+        public string DistrictMunicipalityName { get; set; }
+
+        public int Year { get; set; }
+
+        public double ShapeArea { get; set; }
+
+        public double ShapeLength { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public static IList<WardRecord> LoadFromCsv(string filename)
+        {
+            if (!File.Exists(filename)) throw new FileNotFoundException($"Ward file '{filename}' does not exist");
+
+            using var reader = new StreamReader(filename);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<WardRecord>();
+
+            var result = new List<WardRecord>();
+
+            while (csv.Read())
+            {
+                result.Add(csv.GetRecord<WardRecord>());
+            }
+
+            Log.Information($"Loaded geographic information from {filename}");
+            return result;
+        }
+    }
+}
